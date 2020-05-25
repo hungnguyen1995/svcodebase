342e0ba55386db1c76fafbc2227a9f5773843ffc
Hưng Nguyễn <hungnm@nhanlucsieuviet.com>
2020-05-25 13:34:20 +0700

    init 1.0.4

--- build/index.d.ts
+++ build/index.d.ts
@@ -4,6 +4,6 @@ export { FeCheck } from "./Services/Check";
 export { useOnClickOutside } from "./Hooks/useOutSide";
 export { useHover } from "./Hooks/useHover";
 export { useScrollToTop } from "./Hooks/useScrollToTop";
-export * from "./Hooks/useCommon";
+export { useDispatchAction, dispatchServer, useCommon, useUserInfo, useUserNotice, useUserFavourite, useToggle, useLocation, useApi, useApiResponse, } from "./Hooks/useCommon";
 export { useSticky } from "./Hooks/useSticky";
 export { useDebounce } from "./Hooks/useDebounce";
